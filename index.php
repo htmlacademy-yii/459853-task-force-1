cda36026d22a47ea0ca710e7cd6b97c24067819a
Igor Ch <igorchabanovspb@gmail.com>
2019-11-14 01:19:21 +0300

    Правки согласно замечаниям

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
     // Подключаем autoload
     require_once 'vendor/autoload.php';
 
-    use App\controllers\OrderCycle;
+    use App\Controllers\OrderCycle;
 
-    $task = new OrderCycle(5, 2, 2, date("Y-m-d"), 'create');
+    $task = new OrderCycle(5, 2, 2, date('Y-m-d'), 'create');
     echo '<pre>';
     var_dump($task->getNextStatus($task::ACTIONS[0]));
     var_dump($task->getNextStatus($task::ACTIONS[1]));
--- src/controllers/OrderCycle.php
+++ src/controllers/OrderCycle.php
@@ -1,5 +1,5 @@
 <?php
-    namespace App\controllers;
+    namespace App\Controllers;
 
     class OrderCycle {
         // Actions
